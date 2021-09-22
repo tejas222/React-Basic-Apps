9dec665e1b10fbb00e8b6739473981547e874bda
tejas222 <40661110+tejas222@users.noreply.github.com>
2021-09-22 16:40:40 +0530

    Add files via upload

---
+++ clock/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Clock from "./Clock";
+import Stopwatch from "./Stopwatch";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+}
