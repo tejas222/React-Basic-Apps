f172a3c85b803a135ffe0d979f8676d2a0324d56
tejas222 <40661110+tejas222@users.noreply.github.com>
2023-06-24 08:42:13 +0530

    updated code

--- stopwatch/src/Stopwatch.js
+++ stopwatch/src/Stopwatch.js
@@ -1,60 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 
-export default class Stopwatch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: 0,
-      timeOn: false,
-      timeOff: true,
-    };
-  }
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
-  handleClickStart = () => {
-    if (this.state.timeOff) {
-      this.myInterval = setInterval(() => {
-        this.setState((prevTime) => ({
-          time: prevTime.time + 10,
-          timeOn: true,
-          timeOff: false,
-        }));
-      }, 10);
+  useEffect(() => {
+    let interval = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
     }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStartStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
   };
 
-  handleClickStop = () => {
-    clearInterval(this.myInterval);
-    this.setState({ timeOn: false, timeOff: true });
-    console.log(this.state.timeOn + " " + this.state.timeOff);
+  const handleReset = () => {
+    setTime(0);
+    setIsRunning(false);
   };
 
-  handleClickReset = () => {
-    this.setState({ time: 0 });
-  };
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <div>{time}</div>
+      <button onClick={handleStartStop}>{isRunning ? 'Stop' : 'Start'}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
 
-  render() {
-    const { time } = this.state;
-    return (
-      <div>
-        <h1>
-          {time > 60000 && (
-            <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
-          )}
-          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}s : </span>
-          <span>{("0" + ((time / 10) % 100)).slice(-2)} </span>
-        </h1>
-
-        <button
-          onClick={
-            this.state.timeOff ? this.handleClickStart : this.handleClickStop
-          }
-        >
-          {this.state.timeOn === true ? "Stop" : "Start"}
-        </button>
-
-        {/* <button onClick={this.handleClickStop.bind(this)}>Stop</button> */}
-        <button onClick={this.handleClickReset}> Reset</button>
-      </div>
-    );
-  }
-}
+export default Stopwatch;
